462c0b806827f807f384af0e03ab052c9ade06fd
avilexapro <78136777+avilexapro@users.noreply.github.com>
2022-01-09 21:42:29 +0530

    changes in code

--- auto-join.py
+++ auto-join.py
@@ -1,10 +1,9 @@
 import time
-import pyautogui
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 PATH = 'C:\Program Files (x86)\Chrome Drivers\97.0.4692.71\chromedriver.exe'
 driver = webdriver.Chrome(PATH)
@@ -12,20 +11,38 @@ driver = webdriver.Chrome(PATH)
 driver.get('https://kyc.edmatix.com/login')
 driver.maximize_window()
 
-driver.find_element(By.ID, 'orgcode').send_keys('MLZEES')
-driver.find_element(By.ID, 'mat-input-1').send_keys('9866482000')
-driver.find_element(By.ID, 'mat-input-2').send_keys('9866482000', Keys.ENTER)
+# School Code
+try:  # This waits and checks for 10 seconds for the presence for the element it is better than using Time.Sleep().
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'orgcode')))
+finally:  # After checking the presence of the element then it sends Keys to the intractable element.
+    driver.find_element(By.ID, 'orgcode').send_keys('MLZEES')
 
-try:
+# Username
+try:  # This waits and checks for 10 seconds for the presence for the element it is better than using Time.Sleep().
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mat-input-1')))
+finally:   # After checking the presence of the element then it sends Keys to the intractable element.
+    driver.find_element(By.ID, 'mat-input-1').send_keys('9866482000')
+
+# Password
+try:  # This waits and checks for 10 seconds for the presence for the element it is better than using Time.Sleep().
+    WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, 'mat-input-2')))
+finally:  # After checking the presence of the element then it sends Keys to the intractable element.
+    driver.find_element(By.ID, 'mat-input-2').send_keys('9866482000', Keys.ENTER)
+
+# Avinash profile avatar
+try:  # This waits and checks for 10 seconds for the presence for the element it is better than using Time.Sleep().
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="app"]/header/nav/div/div[1]/ul/li[2]/a')))
-finally:
+finally:  # After checking the presence of the element then it clicks to the intractable element.
     driver.find_element(By.XPATH, '//*[@id="app"]/header/nav/div/div[1]/ul/li[2]/a').click()
 
-try:
+# My Schedule
+try:  # This waits and checks for 10 seconds for the presence for the element it is better than using Time.Sleep().
     WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@id="bs-example-navbar-collapse-1"]/ul[1]/li[10]/button')))
-finally:
+finally:  # After checking the presence of the element then it clicks to the intractable element.
     driver.find_element(By.XPATH, '//*[@id="bs-example-navbar-collapse-1"]/ul[1]/li[10]/button').click()
 
+# Set timer till 3 seconds
 time.sleep(3)
 
+# Quits the browser
 driver.quit()
